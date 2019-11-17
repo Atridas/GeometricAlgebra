395f382d1f255172c35617b5b7a7341b99c16db2
Isaac 'Atridas' Serrano Guasch <atridas87@gmail.com>
2019-11-17 22:16:59 +0000

    Squeleton

--- solution/Examples.cpp
+++ solution/Examples.cpp
@@ -1,20 +1,302 @@
 // GeometricAlgebra.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <type_traits>
 
-int main()
+#include <GeometricAlgebra.hpp>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+template<typename T, typename Q>
+bool IsType(Q q)
 {
-    std::cout << "Hello World!\n";
+	return std::is_same_v<T, Q>;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+namespace test2D {
+	using namespace GA;
+	using Euclidean2D = Signature<double, 3>;
+	using Scalar = BaseVector<Euclidean2D>;
+	using X = BaseVector<Euclidean2D, 0>;
+	using Y = BaseVector<Euclidean2D, 1>;
+	using XY = BaseVector<Euclidean2D, 0, 1>;
+
+	TEST_CASE("2d euclidean multiplication type", "[2d euclidean]") {
+		// scalar * vec
+		REQUIRE(IsType<X>(Scalar{}.MultType(X{})));
+		REQUIRE(IsType<Y>(Scalar{}.MultType(Y{})));
+		REQUIRE(IsType<X>(X{}.MultType(Scalar{})));
+		REQUIRE(IsType<Y>(Y{}.MultType(Scalar{})));
+
+		// scalar * biv
+		REQUIRE(IsType<XY>(Scalar{}.MultType(XY{})));
+		REQUIRE(IsType<XY>(XY{}.MultType(Scalar{})));
+
+		// vec * vec
+		REQUIRE(IsType<Scalar>(X{}.MultType(X{})));
+		REQUIRE(IsType<Scalar>(Y{}.MultType(Y{})));
+		REQUIRE(IsType<XY>(X{}.MultType(Y{})));
+		REQUIRE(IsType<XY>(Y{}.MultType(X{})));
+
+		// vec * biv
+		REQUIRE(IsType<Y>(X{}.MultType(XY{})));
+		REQUIRE(IsType<X>(Y{}.MultType(XY{})));
+		REQUIRE(IsType<Y>(XY{}.MultType(X{})));
+		REQUIRE(IsType<X>(XY{}.MultType(Y{})));
+
+		// biv * biv
+		REQUIRE(IsType<Scalar>(XY{}.MultType(XY{})));
+	}
+
+	TEST_CASE("2d euclidean multiplication sign", "[2d euclidean]") {
+		// scalar * vec
+		REQUIRE(Scalar{}.MultSign(X{}) == 1);
+		REQUIRE(Scalar{}.MultSign(Y{}) == 1);
+		REQUIRE(X{}.MultSign(Scalar{}) == 1);
+		REQUIRE(Y{}.MultSign(Scalar{}) == 1);
+
+		// scalar * biv
+		REQUIRE(Scalar{}.MultSign(XY{}) == 1);
+		REQUIRE(XY{}.MultSign(Scalar{}) == 1);
+
+		// vec * vec
+		REQUIRE(X{}.MultSign(X{}) == 1);
+		REQUIRE(Y{}.MultSign(Y{}) == 1);
+		REQUIRE(X{}.MultSign(Y{}) == 1);
+		REQUIRE(Y{}.MultSign(X{}) == -1);
+
+		// vec * biv
+		REQUIRE(X{}.MultSign(XY{}) == 1);
+		REQUIRE(Y{}.MultSign(XY{}) == -1);
+		REQUIRE(XY{}.MultSign(X{}) == -1);
+		REQUIRE(XY{}.MultSign(Y{}) == 1);
+
+		// biv * biv
+		REQUIRE(XY{}.MultSign(XY{}) == -1);
+	}
+}
+
+
+namespace test3D {
+	using namespace GA;
+	using Euclidean3D = Signature<double, 3>;
+	using Scalar = BaseVector<Euclidean3D>;
+	using X = BaseVector<Euclidean3D, 0>;
+	using Y = BaseVector<Euclidean3D, 1>;
+	using Z = BaseVector<Euclidean3D, 2>;
+	using XY = BaseVector<Euclidean3D, 0, 1>;
+	using XZ = BaseVector<Euclidean3D, 0, 2>;
+	using YZ = BaseVector<Euclidean3D, 1, 2>;
+	using XYZ = BaseVector<Euclidean3D, 0, 1, 2>;
+
+	TEST_CASE("3d euclidean multiplication type", "[3d euclidean]") {
+		// scalar * vec
+		REQUIRE(IsType<X>(Scalar{}.MultType(X{})));
+		REQUIRE(IsType<Y>(Scalar{}.MultType(Y{})));
+		REQUIRE(IsType<Z>(Scalar{}.MultType(Z{})));
+
+		REQUIRE(IsType<X>(X{}.MultType(Scalar{})));
+		REQUIRE(IsType<Y>(Y{}.MultType(Scalar{})));
+		REQUIRE(IsType<Z>(Z{}.MultType(Scalar{})));
+
+
+		// scalar * biv
+		REQUIRE(IsType<XY>(Scalar{}.MultType(XY{})));
+		REQUIRE(IsType<XZ>(Scalar{}.MultType(XZ{})));
+		REQUIRE(IsType<YZ>(Scalar{}.MultType(YZ{})));
+
+		REQUIRE(IsType<XY>(XY{}.MultType(Scalar{})));
+		REQUIRE(IsType<XZ>(XZ{}.MultType(Scalar{})));
+		REQUIRE(IsType<YZ>(YZ{}.MultType(Scalar{})));
+
+
+		// scalar * tri
+		REQUIRE(IsType<XYZ>(Scalar{}.MultType(XYZ{})));
+		REQUIRE(IsType<XYZ>(XYZ{}.MultType(Scalar{})));
+
+
+		// vec * vec
+		REQUIRE(IsType<Scalar>(X{}.MultType(X{})));
+		REQUIRE(IsType<XY>(X{}.MultType(Y{})));
+		REQUIRE(IsType<XZ>(X{}.MultType(Z{})));
+
+		REQUIRE(IsType<XY>(Y{}.MultType(X{})));
+		REQUIRE(IsType<Scalar>(Y{}.MultType(Y{})));
+		REQUIRE(IsType<YZ>(Y{}.MultType(Z{})));
+
+		REQUIRE(IsType<XZ>(Z{}.MultType(X{})));
+		REQUIRE(IsType<YZ>(Z{}.MultType(Y{})));
+		REQUIRE(IsType<Scalar>(Z{}.MultType(Z{})));
+
+
+		// vec * biv
+		REQUIRE(IsType<Y>(X{}.MultType(XY{})));
+		REQUIRE(IsType<Z>(X{}.MultType(XZ{})));
+		REQUIRE(IsType<XYZ>(X{}.MultType(YZ{})));
+
+		REQUIRE(IsType<X>(Y{}.MultType(XY{})));
+		REQUIRE(IsType<XYZ>(Y{}.MultType(XZ{})));
+		REQUIRE(IsType<Z>(Y{}.MultType(YZ{})));
+
+		REQUIRE(IsType<XYZ>(Z{}.MultType(XY{})));
+		REQUIRE(IsType<X>(Z{}.MultType(XZ{})));
+		REQUIRE(IsType<Y>(Z{}.MultType(YZ{})));
+
+		REQUIRE(IsType<Y>(XY{}.MultType(X{})));
+		REQUIRE(IsType<X>(XY{}.MultType(Y{})));
+		REQUIRE(IsType<XYZ>(XY{}.MultType(Z{})));
+
+		REQUIRE(IsType<Z>(XZ{}.MultType(X{})));
+		REQUIRE(IsType<XYZ>(XZ{}.MultType(Y{})));
+		REQUIRE(IsType<X>(XZ{}.MultType(Z{})));
+
+		REQUIRE(IsType<XYZ>(YZ{}.MultType(X{})));
+		REQUIRE(IsType<Z>(YZ{}.MultType(Y{})));
+		REQUIRE(IsType<Y>(YZ{}.MultType(Z{})));
+
+
+		// vec * tri
+		REQUIRE(IsType<YZ>(X{}.MultType(XYZ{})));
+		REQUIRE(IsType<XZ>(Y{}.MultType(XYZ{})));
+		REQUIRE(IsType<XY>(Z{}.MultType(XYZ{})));
+
+		REQUIRE(IsType<YZ>(XYZ{}.MultType(X{})));
+		REQUIRE(IsType<XZ>(XYZ{}.MultType(Y{})));
+		REQUIRE(IsType<XY>(XYZ{}.MultType(Z{})));
+
+
+		// biv * biv
+		REQUIRE(IsType<Scalar>(XY{}.MultType(XY{})));
+		REQUIRE(IsType<YZ>(XY{}.MultType(XZ{})));
+		REQUIRE(IsType<XZ>(XY{}.MultType(YZ{})));
+
+		REQUIRE(IsType<YZ>(XZ{}.MultType(XY{})));
+		REQUIRE(IsType<Scalar>(XZ{}.MultType(XZ{})));
+		REQUIRE(IsType<XY>(XZ{}.MultType(YZ{})));
+
+		REQUIRE(IsType<XZ>(YZ{}.MultType(XY{})));
+		REQUIRE(IsType<XY>(YZ{}.MultType(XZ{})));
+		REQUIRE(IsType<Scalar>(YZ{}.MultType(YZ{})));
+
+
+		// biv * tri
+		REQUIRE(IsType<Z>(XY{}.MultType(XYZ{})));
+		REQUIRE(IsType<Y>(XZ{}.MultType(XYZ{})));
+		REQUIRE(IsType<X>(YZ{}.MultType(XYZ{})));
+
+		REQUIRE(IsType<Z>(XYZ{}.MultType(XY{})));
+		REQUIRE(IsType<Y>(XYZ{}.MultType(XZ{})));
+		REQUIRE(IsType<X>(XYZ{}.MultType(YZ{})));
+
+
+		// tri * tri
+		REQUIRE(IsType<Scalar>(XYZ{}.MultType(XYZ{})));
+	}
+
+	TEST_CASE("3d euclidean multiplication sign", "[3d euclidean]") {
+		// scalar * vec
+		REQUIRE(Scalar{}.MultSign(X{}) == 1);
+		REQUIRE(Scalar{}.MultSign(Y{}) == 1);
+		REQUIRE(Scalar{}.MultSign(Z{}) == 1);
+
+		REQUIRE(X{}.MultSign(Scalar{}) == 1);
+		REQUIRE(Y{}.MultSign(Scalar{}) == 1);
+		REQUIRE(Z{}.MultSign(Scalar{}) == 1);
+
+
+		// scalar * biv
+		REQUIRE(Scalar{}.MultSign(XY{}) == 1);
+		REQUIRE(Scalar{}.MultSign(XZ{}) == 1);
+		REQUIRE(Scalar{}.MultSign(YZ{}) == 1);
+
+		REQUIRE(XY{}.MultSign(Scalar{}) == 1);
+		REQUIRE(XZ{}.MultSign(Scalar{}) == 1);
+		REQUIRE(YZ{}.MultSign(Scalar{}) == 1);
+
+
+		// scalar * tri
+		REQUIRE(Scalar{}.MultSign(XYZ{}) == 1);
+		REQUIRE(XYZ{}.MultSign(Scalar{}) == 1);
+
+
+		// vec * vec
+		REQUIRE(X{}.MultSign(X{}) == 1);
+		REQUIRE(X{}.MultSign(Y{}) == 1);
+		REQUIRE(X{}.MultSign(Z{}) == 1);
+
+		REQUIRE(Y{}.MultSign(X{}) == -1);
+		REQUIRE(Y{}.MultSign(Y{}) == 1);
+		REQUIRE(Y{}.MultSign(Z{}) == 1);
+
+		REQUIRE(Z{}.MultSign(X{}) == -1);
+		REQUIRE(Z{}.MultSign(Y{}) == -1);
+		REQUIRE(Z{}.MultSign(Z{}) == 1);
+
+
+		// vec * biv
+		REQUIRE(X{}.MultSign(XY{}) == 1);
+		REQUIRE(X{}.MultSign(XZ{}) == 1);
+		REQUIRE(X{}.MultSign(YZ{}) == 1);
+
+		REQUIRE(Y{}.MultSign(XY{}) == -1);
+		REQUIRE(Y{}.MultSign(XZ{}) == -1);
+		REQUIRE(Y{}.MultSign(YZ{}) == 1);
+
+		REQUIRE(Z{}.MultSign(XY{}) == 1);
+		REQUIRE(Z{}.MultSign(XZ{}) == -1);
+		REQUIRE(Z{}.MultSign(YZ{}) == -1);
+
+		REQUIRE(XY{}.MultSign(X{}) == -1);
+		REQUIRE(XY{}.MultSign(Y{}) == 1);
+		REQUIRE(XY{}.MultSign(Z{}) == 1);
+
+		REQUIRE(XZ{}.MultSign(X{}) == -1);
+		REQUIRE(XZ{}.MultSign(Y{}) == -1);
+		REQUIRE(XZ{}.MultSign(Z{}) == 1);
+
+		REQUIRE(YZ{}.MultSign(X{}) == 1);
+		REQUIRE(YZ{}.MultSign(Y{}) == -1);
+		REQUIRE(YZ{}.MultSign(Z{}) == 1);
+
+
+		// vec * tri
+		REQUIRE(X{}.MultSign(XYZ{}) == 1);
+		REQUIRE(Y{}.MultSign(XYZ{}) == -1);
+		REQUIRE(Z{}.MultSign(XYZ{}) == 1);
+
+		REQUIRE(XYZ{}.MultSign(X{}) == 1);
+		REQUIRE(XYZ{}.MultSign(Y{}) == -1);
+		REQUIRE(XYZ{}.MultSign(Z{}) == 1);
+
+
+		// biv * biv
+		REQUIRE(XY{}.MultSign(XY{}) == -1);
+		REQUIRE(XY{}.MultSign(XZ{}) == -1);
+		REQUIRE(XY{}.MultSign(YZ{}) == 1);
+
+		REQUIRE(XZ{}.MultSign(XY{}) == 1);
+		REQUIRE(XZ{}.MultSign(XZ{}) == -1);
+		REQUIRE(XZ{}.MultSign(YZ{}) == -1);
+
+		REQUIRE(YZ{}.MultSign(XY{}) == -1);
+		REQUIRE(YZ{}.MultSign(XZ{}) == 1);
+		REQUIRE(YZ{}.MultSign(YZ{}) == -1);
+
+
+		// biv * tri
+		REQUIRE(XY{}.MultSign(XYZ{}) == -1);
+		REQUIRE(XZ{}.MultSign(XYZ{}) == 1);
+		REQUIRE(YZ{}.MultSign(XYZ{}) == -1);
+
+		REQUIRE(XYZ{}.MultSign(XY{}) == -1);
+		REQUIRE(XYZ{}.MultSign(XZ{}) == 1);
+		REQUIRE(XYZ{}.MultSign(YZ{}) == -1);
+
+
+		// tri * tri
+		REQUIRE(XYZ{}.MultSign(XYZ{}) == -1);
+	}
+}
+
--- src/GeometricAlgebra.hpp
+++ src/GeometricAlgebra.hpp
@@ -1,1 +1,139 @@
 #pragma once
+
+namespace GA
+{
+
+	template<typename T, int Positive, int Negative = 0, int Zero = 0>
+	struct Signature
+	{
+		static constexpr int Positive = Positive;
+		static constexpr int Negative = Negative;
+		static constexpr int Zero = Zero;
+
+		static constexpr int NumBaseVectors = Positive + Negative + Zero;
+
+		static constexpr int GetSignature(int index)
+		{
+			if (index < Positive)
+				return 1;
+			else if (index - Positive < Negative)
+				return -1;
+			else if (index - Positive - Negative < Zero)
+				return 0;
+			else
+				return -9999999;
+		}
+
+		static constexpr int NumBase(int grade)
+		{
+			long long int acum = 1;
+			for (long long int i = 0; i < grade; ++i)
+				acum *= NumBaseVectors - i;
+			for (long long int i = 0; i < grade; ++i)
+				acum /= grade - i;
+			return static_cast<int>(acum);
+		}
+
+		template<int N = Positive + Negative + Zero>
+		static constexpr auto Pseudoscalar();
+		static constexpr auto InversePseudoscalar();
+
+		using Type = T;
+	};
+
+
+	template<typename Signature, int... Indexes>
+	struct BaseVector
+	{
+		static_assert(sizeof...(Indexes) == 0);
+
+		static constexpr int Grade = 0;
+
+		constexpr BaseVector() {}
+
+		template<int... ParamIndexes>
+		constexpr BaseVector<Signature, ParamIndexes...> MultType(BaseVector<Signature, ParamIndexes...>) const
+		{
+			return BaseVector<Signature, ParamIndexes...>{};
+		}
+
+		template<int... ParamIndexes>
+		constexpr int MultSign(BaseVector<Signature, ParamIndexes...>) const
+		{
+			return 1;
+		}
+	};
+
+	template<typename Signature, int FirstIndex, int... Indexes>
+	struct BaseVector<Signature, FirstIndex, Indexes...>
+	{
+		static constexpr int Grade = sizeof...(Indexes) + 1;
+
+		static constexpr int First = FirstIndex;
+		using ReducedBaseVector = BaseVector<Signature, Indexes...>;
+
+		constexpr BaseVector() {}
+
+		constexpr BaseVector<Signature, FirstIndex, Indexes...> MultType(BaseVector<Signature> v) const
+		{
+			return BaseVector<Signature, FirstIndex, Indexes...>{};
+		}
+		
+		constexpr int MultSign(BaseVector<Signature> v) const
+		{
+			return 1;
+		}
+
+		template<int FirstParamIndex, int... ParamIndexes>
+		constexpr auto MultType(BaseVector<Signature, FirstParamIndex, ParamIndexes...> v) const;
+		
+		template<int FirstParamIndex, int... ParamIndexes>
+		constexpr int MultSign(BaseVector<Signature, FirstParamIndex, ParamIndexes...> v) const;
+	};
+
+	template<typename Signature, int FirstIndex, int... Indexes>
+	template<int FirstParamIndex, int... ParamIndexes>
+	constexpr auto BaseVector<Signature, FirstIndex, Indexes...>::MultType(BaseVector<Signature, FirstParamIndex, ParamIndexes...> v) const
+	{
+		if constexpr (sizeof...(Indexes) == 0)
+		{
+			if constexpr (FirstIndex == FirstParamIndex)
+				return BaseVector<Signature, ParamIndexes...>{};
+			else if constexpr (FirstIndex < FirstParamIndex)
+				return BaseVector<Signature, FirstIndex, FirstParamIndex, ParamIndexes...>{};
+			else
+			{
+				static_assert(FirstIndex > FirstParamIndex);
+				return BaseVector<Signature, FirstParamIndex>{}.MultType(BaseVector<Signature, FirstIndex>{}.MultType(BaseVector<Signature, ParamIndexes...>{}));
+			}
+		}
+		else
+		{
+			return BaseVector<Signature, FirstIndex>{}.MultType(BaseVector<Signature, Indexes...>{}.MultType(v));
+		}
+	}
+
+	template<typename Signature, int FirstIndex, int... Indexes>
+	template<int FirstParamIndex, int... ParamIndexes>
+	constexpr int BaseVector<Signature, FirstIndex, Indexes...>::MultSign(BaseVector<Signature, FirstParamIndex, ParamIndexes...> v) const
+	{
+		if constexpr (sizeof...(Indexes) == 0)
+		{
+			if constexpr (FirstIndex == FirstParamIndex)
+				return Signature::GetSignature(FirstIndex);
+			else if constexpr (FirstIndex < FirstParamIndex)
+				return 1;
+			else
+			{
+				static_assert(FirstIndex > FirstParamIndex);
+				constexpr int aux = BaseVector<Signature, FirstIndex>{}.MultSign(BaseVector<Signature, ParamIndexes...>{});
+				return -1 * aux * BaseVector<Signature, FirstParamIndex>{}.MultSign(BaseVector<Signature, FirstIndex>{}.MultType(BaseVector<Signature, ParamIndexes...>{}));
+			}
+		}
+		else
+		{
+			constexpr int aux = BaseVector<Signature, Indexes...>{}.MultSign(v);
+			return aux * BaseVector<Signature, FirstIndex>{}.MultSign(BaseVector<Signature, Indexes...>{}.MultType(v));
+		}
+	}
+}
